88274b042914cbfdfd2b31cefde767121e583833
goranknezevic <knezevichgoran@gmail.com>
2018-03-18 09:47:14 +0100

    Conditions are predefined to have +30 seconds between stages

--- TenderManagerUI/src/app/conditions/conditions.component.ts
+++ TenderManagerUI/src/app/conditions/conditions.component.ts
@@ -15,15 +15,16 @@ export class ConditionsComponent implements OnInit {
   ngOnInit() {
     this.conditionForm = this.fb.group(
       {
-        openedForOffersTimestamp: '',
-        closedForOffersTimestamp: '',
-        tenderFinishedTimestamp: ''
       }
     );
   }
 
   onSubmit() {
     const formModel = this.conditionForm.value;
+    const now = new Date();
+    formModel.openedForOffersTimestamp = now;
+    formModel.closedForOffersTimestamp = new Date(now.getTime() + 30*1000);
+    formModel.tenderFinishedTimestamp = new Date(now.getTime() + 60*1000);
     formModel.conditionsId = '1';
     formModel.description = 'Tender description';
     formModel.organizer = 'resource:org.example.biznet.Organizer#1'
